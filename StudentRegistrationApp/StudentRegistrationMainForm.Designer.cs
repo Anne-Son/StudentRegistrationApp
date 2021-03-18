f21df7503398cc9963dbf35d9004f1ed37090a83
sabu-t0060 <sabut@student.douglascollege.ca>
2021-03-18 14:18:24 -0700

    datagridview

--- StudentRegistrationApp/StudentRegistrationMainForm.Designer.cs
+++ StudentRegistrationApp/StudentRegistrationMainForm.Designer.cs
@@ -60,7 +60,7 @@ namespace StudentRegistrationApp
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(658, 28);
+            this.label2.Location = new System.Drawing.Point(756, 28);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(89, 17);
             this.label2.TabIndex = 1;
