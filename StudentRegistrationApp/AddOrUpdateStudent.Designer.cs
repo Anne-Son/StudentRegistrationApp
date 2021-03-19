5a70f8b4f91f1906752a74513b37744e2500a5ba
Anne Son <annesontech@gmail.com>
2021-03-18 22:52:00 -0700

    AddOrUpdateDepartment and AddOrUpdateStudent added

---
+++ StudentRegistrationApp/AddOrUpdateStudent.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace StudentRegistrationApp
+{
+    partial class AddOrUpdateStudentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelStudents = new System.Windows.Forms.Label();
+            this.listBoxStudents = new System.Windows.Forms.ListBox();
+            this.listBoxDepartments = new System.Windows.Forms.ListBox();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.labelFirstName = new System.Windows.Forms.Label();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.labelDepartment = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelStudents
+            // 
+            this.labelStudents.AutoSize = true;
+            this.labelStudents.Location = new System.Drawing.Point(44, 32);
+            this.labelStudents.Name = "labelStudents";
+            this.labelStudents.Size = new System.Drawing.Size(64, 17);
+            this.labelStudents.TabIndex = 0;
+            this.labelStudents.Text = "Students";
+            // 
+            // listBoxStudents
+            // 
+            this.listBoxStudents.FormattingEnabled = true;
+            this.listBoxStudents.ItemHeight = 16;
+            this.listBoxStudents.Location = new System.Drawing.Point(200, 32);
+            this.listBoxStudents.Name = "listBoxStudents";
+            this.listBoxStudents.Size = new System.Drawing.Size(255, 244);
+            this.listBoxStudents.TabIndex = 1;
+            // 
+            // listBoxDepartments
+            // 
+            this.listBoxDepartments.FormattingEnabled = true;
+            this.listBoxDepartments.ItemHeight = 16;
+            this.listBoxDepartments.Location = new System.Drawing.Point(232, 379);
+            this.listBoxDepartments.Name = "listBoxDepartments";
+            this.listBoxDepartments.Size = new System.Drawing.Size(191, 148);
+            this.listBoxDepartments.TabIndex = 2;
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(232, 300);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(191, 22);
+            this.textBoxFirstName.TabIndex = 3;
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(232, 334);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(191, 22);
+            this.textBoxLastName.TabIndex = 4;
+            // 
+            // labelFirstName
+            // 
+            this.labelFirstName.AutoSize = true;
+            this.labelFirstName.Location = new System.Drawing.Point(44, 300);
+            this.labelFirstName.Name = "labelFirstName";
+            this.labelFirstName.Size = new System.Drawing.Size(76, 17);
+            this.labelFirstName.TabIndex = 5;
+            this.labelFirstName.Text = "First Name";
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Location = new System.Drawing.Point(44, 334);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(76, 17);
+            this.labelLastName.TabIndex = 6;
+            this.labelLastName.Text = "Last Name";
+            // 
+            // labelDepartment
+            // 
+            this.labelDepartment.AutoSize = true;
+            this.labelDepartment.Location = new System.Drawing.Point(44, 379);
+            this.labelDepartment.Name = "labelDepartment";
+            this.labelDepartment.Size = new System.Drawing.Size(82, 17);
+            this.labelDepartment.TabIndex = 7;
+            this.labelDepartment.Text = "Department";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(200, 549);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(80, 31);
+            this.buttonAdd.TabIndex = 8;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(378, 549);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(77, 31);
+            this.buttonUpdate.TabIndex = 9;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            // 
+            // AddOrUpdateStudentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 609);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.labelDepartment);
+            this.Controls.Add(this.labelLastName);
+            this.Controls.Add(this.labelFirstName);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Controls.Add(this.listBoxDepartments);
+            this.Controls.Add(this.listBoxStudents);
+            this.Controls.Add(this.labelStudents);
+            this.Name = "AddOrUpdateStudentForm";
+            this.Text = "Add Or Update Student";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelStudents;
+        private System.Windows.Forms.ListBox listBoxStudents;
+        private System.Windows.Forms.ListBox listBoxDepartments;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.Label labelFirstName;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.Label labelDepartment;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonUpdate;
+    }
+}
